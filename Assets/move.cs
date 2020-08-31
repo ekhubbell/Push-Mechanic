2646efaa0d9f42aa38bf4947fac6bd8b69d1a446
Emma <ekhubbell@qu.edu>
2020-08-31 14:43:12 -0400

    post of entire project

---
+++ Assets/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    float x,y;
+    void Update()
+    {
+        x = 0.0f;
+        y = 0.0f;
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            x = -speed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            x = speed;
+         }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            y = speed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            y = -speed;
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(x, y);
+    }
+}
---
+++ Assets/push.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    bool continueMoving = false;
+    float countDown;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        continueMoving = true;
+        countDown = .5f;
+    }
+    private void Update()
+    {
+        if (continueMoving)
+        {
+            countDown -= Time.deltaTime;
+            if (countDown <= 0.0f)
+            {
+                continueMoving = false;
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+        }
+    }
+}
